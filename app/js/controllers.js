f09ba683e98764a1261151eca04ae167f77543f6
Stephan Yu <Stephan.yu@gmail.com>
2015-07-17 16:11:52 +0200

    init commit

---
+++ app/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', [])
+  .controller('LandingPageCtrl', [function() {
+    
+  }])
+  .controller('WaitlistCtrl', [function() {
+    
+  }]);
